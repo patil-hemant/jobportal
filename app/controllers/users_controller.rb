82ed7c9f6a791ae4bf1be0291364c96e0b607d3f
patil-hemant <hemant8905@gmail.com>
2014-10-29 11:25:26 +0530

    added edit method.....................

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,25 @@
 class UsersController < ApplicationController
 	
-
-      def new
+    def new
         @user=User.new
-      
-      end
+    end
 
-     
+    def edit
+        @user=User.find(params[:id])
+    end
 
-      def create
-        @user=User.new(user_params)
-        if @user.save
+    def update
+        @user=User.find(params[:id])
+        if @user.update(user_params)
           redirect_to new_user_educationdetail_path(@user.id), notice: 'user was successfully created.'
         else
-          render :action=>:new
+          render :action=>:edit
         end
-
-      end
-
+    end
        
-     
-private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :gender, :contact_no, :address, :skills, :twitter_url, :linkedin_url, :blog_url, :current_location, :ready_to_relocate, :image, :resume)
-  end
+        
+    private
+        def user_params
+        params.require(:user).permit(:first_name, :last_name, :gender, :contact_no, :address, :skills, :twitter_url, :linkedin_url, :blog_url, :current_location, :ready_to_relocate, :image, :resume)
+    end
 end
